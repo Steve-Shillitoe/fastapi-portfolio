36cf02d5f00588113ec894290310cdc1223e3f67
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-02-23 13:38:29 +0000

    Add function to initial PostreSQL DB on start-up

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from routers import artworks
+from database.database import init_db
 
 app = FastAPI()
+
+app.include_router(artworks.router)
+
+
+@app.on_event("startup")
+async def on_startup():
+    await init_db()
+
 
 @app.get("/")
 async def home():
