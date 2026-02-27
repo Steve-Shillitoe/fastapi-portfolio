09cf9c7a9abac7bf2a75fc5f4eb82de67b7fa7b5
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-02-27 10:14:59 +0000

    Create tag database table

---
+++ models/tag.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy.orm import relationship, Mapped, mapped_column
+from database.database import Base
+from typing import List,  TYPE_CHECKING
+if TYPE_CHECKING:
+    from models.artwork import Artwork
+
+artwork_tags = Table(
+    "artwork_tags",
+    Base.metadata,
+    Column("artwork_id", ForeignKey("artworks.id"), primary_key=True),
+    Column("tag_id", ForeignKey("tags.id"), primary_key=True),
+)
+
+class Tag(Base):
+    __tablename__ = "tags"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+
+    artworks: Mapped[List["Artwork"]] = relationship(
+        back_populates="tags",
+        secondary=artwork_tags,
+    )
