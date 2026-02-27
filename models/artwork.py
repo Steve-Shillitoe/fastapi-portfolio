100c855cb922f7f68abf9f4e583f5f077749e84c
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2026-02-27 10:16:28 +0000

    Update table to include tags

--- models/artwork.py
+++ models/artwork.py
@@ -2,11 +2,15 @@
 Artwork ORM model using SQLAlchemy 2.x style mappings.
 """
 
-from typing import Optional
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String, Text
 from database.database import Base
+from models.tag import artwork_tags
+from sqlalchemy.orm import relationship
+from typing import List,  TYPE_CHECKING, Optional
 
+if TYPE_CHECKING:
+    from models.tag import Tag
 
 class Artwork(Base):
     """
@@ -22,6 +26,11 @@ class Artwork(Base):
     image_filename: Mapped[str] = mapped_column(String(300), nullable=False)
 
     comments: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+
+    tags: Mapped[List["Tag"]] = relationship(
+    secondary=artwork_tags,
+    back_populates="artworks",
+)
 
     @property
     def image_url(self) -> str:
